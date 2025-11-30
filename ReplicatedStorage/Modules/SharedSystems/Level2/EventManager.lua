607025a1acbc578aa99a72fd7c446bec00bc8472
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 10:06:52 -0600

    Updated ReplicatedStorage/Modules/SharedSystems/Level2/EventManager.lua

--- ReplicatedStorage/Modules/SharedSystems/Level2/EventManager.lua
+++ ReplicatedStorage/Modules/SharedSystems/Level2/EventManager.lua
@@ -1,5 +1,11 @@
 -- @ScriptType: ModuleScript
+--[[
+Novus, omni_novus, 11/30/25
+EventManager
+Handles event firing and such.
+]]
 
+--Instances
 local Modules = script.Parent.Parent
 
 local Level1 = Modules.Level1
@@ -14,6 +20,9 @@ local EventManager = {}
 local EventObject = {}
 EventObject.__index = EventObject
 
+--Types
+
+
 --@private
 type DefaultArg = ()->()
 
@@ -27,16 +36,22 @@ type DefaultEventList = {
 
 export type Event<Args,Func> = {
 	Args:Args|DefaultArg,
-	Func:Func|DefaultFunc
+	Func:Func|DefaultFunc,
+	Waiting:{thread}
 }
 export type EventObject<EventList,Args,Func> = {
 	List:EventList,
+	
 
 }
 
 
 export type DefaultEventObject = EventObject<DefaultEventList,
 DefaultArg,DefaultFunc>
+
+export type DefaultEvent = Event<
+DefaultArg,DefaultFunc>
+
 
 local DefaultArgsFunc:DefaultArg = function()
 
@@ -65,7 +80,8 @@ function EventManager.CreateEvent<T2,T3>(Func:T2,ReturnArgs:T3|DefaultArg)
 	end
 	local Event:Event<T3,T2> = {
 		Args = ReturnArgs,
-		Func = Func
+		Func = Func,
+		Waiting = {}
 	}
 	return Event
 	end
@@ -112,10 +128,13 @@ function EventObject._Fire<SelfType, EventNameType>(
 	type ListType = typeof(self.List)
 	
 	if self.List[EventName] then
-		local Event = self.List[EventName]
+		local Event:DefaultEvent = self.List[EventName]
+		for i,v in Event.Waiting do
+			pcall(coroutine.resume,v)
+		end
 		return ThreadManager:CreateThread(nil,function()
 
-			return self.List[EventName].Func(self.List[EventName].Args())
+				return Event.Func(Event.Args())
 		end)
 	end
 	--end
@@ -145,11 +164,42 @@ function EventObject._Append<SelfType,T1>(self:SelfType,Funcs:T1)
 	end
 	return self
 end
-function EventObject:Append<T1>(Funcs:T)
+function EventObject:Append<T1>(Funcs:T1)
 	local NewObject = EventObject._Append(self,Funcs)
-	local self:typeof(NewObject)  = NewObject
 	
+	return NewObject
+end
+
+
+
+--@private
+function EventObject._WaitUntil<SelfType,T1>(self:SelfType,Event:T1)
+	local self:SelfType&DefaultEventObject = self
+	
+	
+	if typeof(Event) == "string" and self.List[Event] then
+		local FoundEvent:DefaultEvent = self.List[Event]
+		
+		
+		local Thr = coroutine.running()
+		local Found = table.find(FoundEvent.Waiting,Thr)
+		if not Found then
+			table.insert(FoundEvent.Waiting,Thr)
+		end
+		coroutine.yield(Thr)
+		
+		return self
+	end
 	return self
 end
+
+function EventObject:WaitUntil<T1>(Event:T1)
+	local NewObject = EventObject._WaitUntil(self,Event)
+
+	return NewObject
+end
+
+
+
 
 return EventManager
