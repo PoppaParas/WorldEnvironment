e6baea43ab26d7f3d81ce57e60f4080fa7f877e9
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 04:36:16 -0600

    Updated ServerStorage/WorldContainer/Internals/Base/ContainerManager.lua

---
+++ ServerStorage/WorldContainer/Internals/Base/ContainerManager.lua
@@ -0,0 +1,27 @@
+-- @ScriptType: ModuleScript
+
+
+local ContainerManager = {}
+local ContainerObject = {}
+ContainerObject.__index = ContainerObject
+
+type UpdateFuncType<ValType> = (Property:string,Value:ValType)->any
+
+export type ContainerAccessClass = "Public"|"Private"
+
+export type Container = {
+	Class:ContainerAccessClass
+} & typeof(setmetatable({},ContainerObject))
+
+function ContainerManager.new():Container
+	local self:Container = setmetatable({
+		
+	},ContainerObject)
+	
+	self.Containers = {}
+	
+	return self
+	
+end
+
+return ContainerManager
