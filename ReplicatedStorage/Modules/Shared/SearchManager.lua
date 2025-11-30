2bba9f45c1a5f551511126af2fc3f8ecb695f7d5
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 04:57:10 -0600

    Updated ReplicatedStorage/Modules/Shared/SearchManager.lua

---
+++ ReplicatedStorage/Modules/Shared/SearchManager.lua
@@ -0,0 +1,13 @@
+-- @ScriptType: ModuleScript
+local SearchManager = {}
+
+function ReturnObject<T1>(Index:type)
+	
+	
+end
+
+function SearchManager:FindObject<T1>(Index:T1)
+	
+end
+
+return SearchManager
