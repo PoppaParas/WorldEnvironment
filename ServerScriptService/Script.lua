76cdce800cd84c0904400bb252a63de2793e8fa5
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 09:41:22 -0600

    Updated ServerScriptService/Script.lua

--- ServerScriptService/Script.lua
+++ ServerScriptService/Script.lua
@@ -6,4 +6,26 @@ local Modules = RepStorage:WaitForChild("Modules")
 local SharedSystems = require(Modules:WaitForChild("SharedSystems"))
 task.wait()
 
-print(SharedSystems.Level1.IndexManager.ObjectIndexes)
+local Events = SharedSystems.Modules.EventManager
+local TestEvent = Events.CreateEvent(function()
+		print("Test")
+		return "True"
+end,function()
+	
+end)
+local NewObj = Events.new({
+	Test = TestEvent
+})
+
+local Amnt = 0
+local NewObj = NewObj:Append(
+	{
+		Test2 = Events.CreateEvent(function(Arg1:number)
+			print(Arg1,"Shots")
+		end,function()
+			Amnt += 1
+			return Amnt
+		end)
+	}
+)
+
