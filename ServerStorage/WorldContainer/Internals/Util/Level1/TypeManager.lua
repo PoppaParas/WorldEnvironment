cd2005170e2713aff26721ddd612e3ad82813a3c
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 04:36:14 -0600

    Updated ServerStorage/WorldContainer/Internals/Util/Level1/TypeManager.lua

---
+++ ServerStorage/WorldContainer/Internals/Util/Level1/TypeManager.lua
@@ -0,0 +1,14 @@
+-- @ScriptType: ModuleScript
+
+local TypeManager = {}
+local TypeObject = {}
+TypeObject.__index = TypeObject
+
+function TypeManager:RegisterType<T>(Object:T)
+	
+end
+
+
+
+
+return TypeManager
