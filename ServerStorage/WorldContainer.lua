625d352075b37a741d15896bd1fc877510c5db0e
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 04:36:15 -0600

    Updated ServerStorage/WorldContainer.lua

---
+++ ServerStorage/WorldContainer.lua
@@ -0,0 +1,49 @@
+-- @ScriptType: ModuleScript
+
+local Internals = script.Internals
+
+local Core = Internals.Core
+local Util = Internals.Util
+local Base = Internals.Base
+
+local UtilLevel2 = Util.Level2
+
+local Container = require(Base.ContainerManager)
+local FunctionStack = require(UtilLevel2.FunctionStack)
+
+local StackObject = FunctionStack:Register({
+	Lol = function()
+		
+	end,
+})
+
+
+local StackObject2 = StackObject:Register({
+	Lol2 = function()
+		
+	end,
+})
+
+local StackObject3 = StackObject2:Register({
+	SuperMarioBros = function()
+		
+	end,
+})
+
+
+
+
+
+
+local WorldManager = {}
+
+export type Environment = {
+	
+}
+
+function WorldManager:CreateEnvironment()
+	
+end
+
+
+return WorldManager
