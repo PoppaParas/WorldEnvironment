4ebe4cd591f3932edb0abd7b0369321b4f09ba71
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 04:36:20 -0600

    Updated ServerStorage/WorldContainer/Internals/Base/ClassManager.lua

---
+++ ServerStorage/WorldContainer/Internals/Base/ClassManager.lua
@@ -0,0 +1,28 @@
+-- @ScriptType: ModuleScript
+local ClassManager = {}
+
+local ClassTemplate = {}
+ClassTemplate.__index = ClassTemplate
+
+--The class object
+local NewClass = {}
+NewClass.__index = NewClass
+
+--The current typelisting table for any class
+local ClassList = {}
+
+
+--Class manager methods
+
+--[[
+Create new class object
+]]
+function ClassManager:CreateTemplate<T,TemplateType>(NewInstance:T,Template:TemplateType)
+	
+end
+
+-- Class template methods
+
+
+
+return ClassManager
