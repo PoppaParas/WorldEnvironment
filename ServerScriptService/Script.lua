bf323a79ad2d3178793ebfd42c5d0cfddf998bfb
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 06:34:11 -0600

    Updated ServerScriptService/Script.lua

--- ServerScriptService/Script.lua
+++ ServerScriptService/Script.lua
@@ -4,4 +4,6 @@ local RepStorage = game:GetService("ReplicatedStorage")
 local Modules = RepStorage:WaitForChild("Modules")
 
 local SharedSystems = require(Modules:WaitForChild("SharedSystems"))
-print(SharedSystems.Level1.IndexManager)
+task.wait()
+
+print(SharedSystems.Level1.IndexManager.ObjectIndexes)
