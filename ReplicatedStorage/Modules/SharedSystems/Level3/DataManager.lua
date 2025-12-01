9df95b19dd3f711d2ad5be491ac1f2a5f9baa550
Paras <167920376+PoppaParas@users.noreply.github.com>
2025-11-30 21:00:10 -0600

    Updated ReplicatedStorage/Modules/SharedSystems/Level3/DataManager.lua

--- ReplicatedStorage/Modules/SharedSystems/Level3/DataManager.lua
+++ ReplicatedStorage/Modules/SharedSystems/Level3/DataManager.lua
@@ -6,19 +6,34 @@ DataManager
 Handles data manipulation, reading, saving, etc.
 ]]
 
+local Libraries = script.Parent.Parent
+
+local Utils = Libraries.Utils
+local Level1 = Libraries.Level1
+local Level2 = Libraries.Level2
+
+local ListManager = require(Utils.List)
+local TableUtil = require(Utils.TableUtil)
+
+local ObjectList:ListManager.ListObjectType<Instance,DataObject<Instance>> = ListManager.new()
+
 local DataManager = {
 	Config = {}
 }
 
 local DataObject = {}
 DataObject.__index = DataObject
-
-
-
-
-
-
 local Data = require(script.Data)
+
+export type DataObject<T1> = {
+	Data:typeof(Data),
+	Object:T1
+} & typeof(DataObject.__index)
+
+
+
+
+
 
 
 
@@ -31,9 +46,45 @@ if true then
 	end
 end
 
-type DataManager = typeof({
-	Config = Data}) & typeof(DataManager)
-local DataManager = DataManager :: DataManager
 
+
+function CreateDataObject<T1,T2>(Object:T1,Override:T2)
+	
+	local New:{
+		Data:T2	
+	} & DataObject<T1>  = {
+		Data = Override,
+		Object = Object
+	}
+	TableUtil:Overwrite(New.Data,DataManager.Config)
+	local New:typeof(New) = New
+	return New
+	
+end
+
+
+function DataManager.new<T1,T2>(Object:T1,Override:T2 & Data)
+	local List = ObjectList:Find(Object)
+	if not List then
+		local NewList = CreateDataObject(Object,Override)
+		ObjectList:Set(Object,NewList)
+		List = NewList :: typeof(NewList)
+	else
+		TableUtil:Overwrite(List.Data,Override) 
+		List = List :: {
+			Data:T2 & typeof(List.Data)
+		} & typeof(List)
+	end
+	
+	
+	return List
+end
+
+
+type DataManager = typeof(DataManager) & {
+	Config:typeof(Data)
+}
+
+local DataManager:DataManager = DataManager
 
 return DataManager
